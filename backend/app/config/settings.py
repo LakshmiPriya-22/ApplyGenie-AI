ab683e0aecf8532bb1c39804b145317bf5159fc8
LakshmiPriya-22 <lakshmipriya6177@gmail.com>
2026-07-20 22:24:15 +0530

    feat(config): add application configuration

---
+++ backend/app/config/settings.py
@@ -0,0 +1,21 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    APP_NAME: str
+    VERSION: str
+    ENVIRONMENT: str
+
+    DB_USER: str
+    DB_PASSWORD: str
+    DB_HOST: str
+    DB_PORT: int
+    DB_NAME: str
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        extra="ignore"
+    )
+
+
+settings = Settings()
